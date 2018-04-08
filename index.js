1a70a19bee32576bb4e1179fbb0603d669397065
NicolasGerhardt <nicolas.gerhardt@gmail.com>
2018-04-07 21:34:07 -0400

    got iframe to work and display a canvas for p5.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-  $("#loaded").text("jQuery Loaded!");
+  
 });
+
+
---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+function setup() {
+	createCanvas(800,600);
+	background(53);
+}
+
+function draw() {
+	stroke(255);
+	
+	line(0,0,width, height);
+	line(width,0,0, height);
+
+}
